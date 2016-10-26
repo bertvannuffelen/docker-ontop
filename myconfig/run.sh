28dd39d83b467f8eca6415ca08754879b4ad1c4f
Bert Van Nuffelen <Bert.vannuffelen@gmail.com>
2016-10-26 12:09:00 +0000

    make cloud enabled post config possible 	modified:   myconfig/run.sh

--- myconfig/run.sh
+++ myconfig/run.sh
@@ -3,6 +3,11 @@
 # for some reason docker does not execute the RUN cp operation not before the entryppoint is called
 cp /myconfig/* /config
 
+# make cloud enabled configuration possible
+if [ -f /data/tomcat-setenv.sh ] ; then 
+    cp /data/tomcat-setenv.sh /usr/share/tomcat7/bin/setenv.sh
+fi
+
 if [ ! -f /webapps/ontop.war ] ; then
    if [ -f /download ] ; then 
    	cp /download /webapps/ontop.zip
